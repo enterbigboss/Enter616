e82dc8d44de3d898ecc040694d540c8208059b36
enterbigboss <ataullahmd787@gmail.com>
2025-03-14 14:01:49 +0300

    Update adbot.js

--- Priyansh/commands/adbot.js
+++ Priyansh/commands/adbot.js
@@ -2,7 +2,7 @@ module.exports.config = {
     name: "ckbot",
     version: "1.0.0",
     hasPermssion: 0,
-    credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+    credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 ",
     description: "DESCRIPTION ABOUT BOT",
     commandCategory: "Media",
     usages: "",
