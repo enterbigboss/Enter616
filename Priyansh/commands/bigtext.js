07e0315a003635076e8860138724d00db58caa13
enterbigboss <ataullahmd787@gmail.com>
2025-03-15 19:07:08 +0300

    Update bigtext.js

--- Priyansh/commands/bigtext.js
+++ Priyansh/commands/bigtext.js
@@ -2,7 +2,8 @@ module.exports.config = {
     name: "bigtext",
     version: "1.0.0",
     hasPermssion: 0,
-    credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+    credits: "𝙀𝙣𝙩𝙚𝙧 𝙗𝙞𝙜𝙗𝙤𝙨𝙨
+        ",
     description: "big text",
     commandCategory: "game",
     usages: "bigtext <text>",
