37818f970dc3097cc7303aa37c84185cd36e2b57
enterbigboss <ataullahmd787@gmail.com>
2025-03-15 18:40:23 +0300

    Update help.js

--- Priyansh/commands/help.js
+++ Priyansh/commands/help.js
@@ -2,7 +2,8 @@
 	name: "help",
 	version: "1.0.2",
 	hasPermssion: 0,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+	credits: "𝙀𝙣𝙩𝙚𝙧 𝙗𝙞𝙜𝙗𝙤𝙨𝙨
+		",
 	description: "Beginner's Guide",
 	commandCategory: "system",
 	usages: "[Tên module]",
