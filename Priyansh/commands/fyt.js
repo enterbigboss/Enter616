0cc0c8ef6270b6c7802be836e4db29884454f4bd
enterbigboss <ataullahmd787@gmail.com>
2025-03-15 19:04:51 +0300

    Update fyt.js

--- Priyansh/commands/fyt.js
+++ Priyansh/commands/fyt.js
@@ -2,7 +2,8 @@ module.exports.config = {
     name: "fyt",
     version: "1.0.0",
     hasPermssion: 2,
-    credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+    credits: "𝙀𝙣𝙩𝙚𝙧 𝙗𝙞𝙜𝙗𝙤𝙨𝙨
+        ",
     description: "War In Chatbox",
     commandCategory: "wargroup",
     usages: "[fyt]",
